c4078e6f8a791ae730cb88b5073420cc70e07394
s1030905 <s1030905@outlook.com>
2023-06-07 10:37:48 +0800

    test

--- _helpers.js
+++ _helpers.js
@@ -3,11 +3,15 @@ function getUser (req) {
   return req.user
 }
 
-module.exports = {
-  getUser
-}
-
 // use helpers.getUser(req) to replace req.user
+
 // function authenticated (req, res, next) {
 //   // passport.authenticate('jwt', { ses...
+//   authenticated
 // }
+
+const ensureAuthenticated = req => {
+  return req.isAuthenticated()
+}
+
+module.exports = { getUser, ensureAuthenticated }
